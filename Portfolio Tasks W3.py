42922194f468e2c50284738dc6d29ba682d3b0a6
bobby <mckeownbobby4@gmail.com>
2023-10-12 15:42:48 +0100

    worked through 5 tasks

--- Portfolio Tasks W3.py
+++ Portfolio Tasks W3.py
@@ -1,10 +1,21 @@
+
 # Task 1
+Name = input("Please enter your name: ")
+if len(Name) > 1:
+    print("hello", Name)
+else:
+    print("Hello stranger ")
 
+# Task 2, 3 ,4
+i = 1
+while i == 1:
+    BadPasswords = ["Password", "password"]
+    NewPassword = input("Please enter your new password")
+    if NewPassword not in BadPasswords:
+        if 13 > len(NewPassword) > 7:
+            PasswordCheck = input("Please re enter your password ")
+            if NewPassword == PasswordCheck:
+                print("Your new password is set! ")
+                i = i + 1
 
-# Task 2
-
-
-# Task 3
-
-
-# Task 4
+# Task 5
