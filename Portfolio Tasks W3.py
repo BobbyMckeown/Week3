a36ac67059557e8b2770bb4c95c8c4dcb66a419b
bobby <mckeownbobby4@gmail.com>
2023-10-12 16:22:44 +0100

    Finnished the last porfolio tasks

--- Portfolio Tasks W3.py
+++ Portfolio Tasks W3.py
@@ -1,4 +1,3 @@
-
 # Task 1
 Name = input("Please enter your name: ")
 if len(Name) > 1:
@@ -6,7 +5,7 @@ if len(Name) > 1:
 else:
     print("Hello stranger ")
 
-# Task 2, 3 ,4
+# Task 2, 3 ,4, 5
 i = 1
 while i == 1:
     BadPasswords = ["Password", "password"]
@@ -15,7 +14,23 @@ while i == 1:
         if 13 > len(NewPassword) > 7:
             PasswordCheck = input("Please re enter your password ")
             if NewPassword == PasswordCheck:
-                print("Your new password is set! ")
                 i = i + 1
+print("Your new password is set! ")
 
-# Task 5
+# Task 6, 7, 8
+temp = 14
+Multiplication = int(input("Please enter the times table you would like to see: "))
+if Multiplication < 0:
+    a = 12
+    while a >= 1:
+        print(Multiplication, "X", a, "=", Multiplication * a)
+        a = a - 1
+else:
+    for i in range(13):
+        maths = i * Multiplication
+        print(maths)
+
+
+
+        maths = i * Multiplication
+        print(i, " x ", Multiplication, " = ", maths)
